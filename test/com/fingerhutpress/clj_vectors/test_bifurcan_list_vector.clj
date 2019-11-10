ed5e9567d822ae0e222b1a76a0916f3cfe9fe2fb
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2019-11-10 15:32:24 -0800

    Add wrapper code and tests for Bifurcan List4 class

--- test/com/fingerhutpress/clj_vectors/test_bifurcan_list_vector.clj
+++ test/com/fingerhutpress/clj_vectors/test_bifurcan_list_vector.clj
@@ -7,8 +7,7 @@
             [collection-check.core :as cc]
             [com.fingerhutpress.clj-vectors.utils :as utils]
             [com.fingerhutpress.clj-vectors.bifurcan-list-vector :as bl]
-            ;;[com.fingerhutpress.clj-vectors.bifurcan-list-vector4 :as bl4]
-            ))
+            [com.fingerhutpress.clj-vectors.bifurcan-list-vector4 :as bl4]))
 
 (set! *warn-on-reflection* true)
 
@@ -45,6 +44,36 @@
   (println "assert-vector-like for Bifurcan List(32) with num-collection-check-tests="
            num-collection-check-tests)
   (cc/assert-vector-like num-collection-check-tests empty-vector-constant gen/int))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(def empty-vector-constant4 (bl4/vector))
+
+(defn seq->vec4 [s]
+  (into empty-vector-constant4 s))
+
+(def test-config4
+  {:seq->vec seq->vec4
+   :test-subvec bl4/subvec
+   :test-catvec bl4/catvec
+   :same-coll? utils/same-coll?
+   :height bl4/height
+   :max-supported-height bl4/max-supported-height
+   :lg-fullness bl4/lg-fullness})
+
+(deftest test-common-bifurcan4
+  (println "Details of Bifurcan List4 implementation:")
+  (bl4/print-library-info)
+  (tc/test-all-common test-config4))
+
+(deftest test-long-bifurcan4
+  (tl/test-all-long test-config4))
+
+(deftest test-vector-like4
+  (println "assert-vector-like for Bifurcan List4 with num-collection-check-tests="
+           num-collection-check-tests)
+  (cc/assert-vector-like num-collection-check-tests empty-vector-constant4 gen/int))
+
 
 
 (comment
